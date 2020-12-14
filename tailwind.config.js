2432a5f54956cc3573c3e324e7d0cb3f68620851
mstanciu552 <mstanciu552@gmail.com>
2020-12-14 23:36:56 +0200

    Added another section and custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange-custom": "#f7a55f",
+        "pale-blue": "#395671",
+      },
+      spacing: {
+        100: "30rem",
+      },
+    },
   },
   variants: {
     extend: {},
