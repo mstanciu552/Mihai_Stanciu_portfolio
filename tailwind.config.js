4a2eef8cbde049d70d931b6c2a65f989dd697522
mstanciu552 <mstanciu552@gmail.com>
2020-12-15 16:20:24 +0200

    Added cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         twitter: "#55ACEE",
         facebook: "#3B5998",
         linkedin: "#007AB9",
+        "gray-orange": "#FEF2E8",
       },
       spacing: {
         100: "30rem",
